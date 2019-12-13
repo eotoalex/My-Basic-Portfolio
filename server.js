525f803b1c8cbd32d2067e49fad81302437b0515
Alex Puello <apuello88@yahoo.com>
2019-12-13 15:08:54 -0500

    renamed the asset file to the public file.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/assets")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get('/', navBarController.loadHomePage);
 
