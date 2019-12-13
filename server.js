be2547b2661e933766910092c05cf0a8ec512db3
Alex Puello <apuello88@yahoo.com>
2019-12-13 13:35:52 -0500

    added a server with routes but the routes do not seem to work with the css pages that are linked to the html.

--- server.js
+++ server.js
@@ -13,11 +13,31 @@ app.use(express.json());
 // app.use(express.static(path.join(_dirname, "client/build")))  (might need this later.)
 
 app.get('/', (req,res) => {
-    res.sendFile("/index.html", (err) => {
+    res.sendFile(path.join(__dirname,"./index.html"), (err) => {
         if(err){
             console.log(err);
         } else {
             console.log('sent index file');
+        };
+    });
+});
+
+app.get('/portfolio', (req, res) => {
+    res.sendFile(path.join(__dirname, "/portfolio.html"), (err) => {
+        if (err){
+            console.log(err);
+        } else {
+            console.log("sent portfolio");
+        };
+    });
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, "/contact.html"), (err) => {
+        if (err){
+            console.log(err);
+        } else {
+            console.log("sent portfolio");
         };
     });
 });
